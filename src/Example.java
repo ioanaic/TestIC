027f9dcac01b5d283fbca3af3a5924aeaf1a9642
flo <flo.macicasan@gmail.com>
2017-02-25 12:03:03 +0200

    adds sum with for that decreases

--- src/Example.java
+++ src/Example.java
@@ -13,11 +13,11 @@ public class Example {
         int y = SkeletonJava.readIntConsole("y=");
 
         int sum = x;
-        int counter = y;
-
-        while(counter > 0) {
+//        int counter = y;
+        //while(counter > 0) {
+        for(int counter = y; counter > 0; counter--) {
             sum = sum + 1;
-            counter -= 1;
+//            counter -= 1;
         }
         SkeletonJava.printConsole(x + "+" + y + " = " + sum);
     }
